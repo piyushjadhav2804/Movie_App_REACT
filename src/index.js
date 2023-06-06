fe3042da74ab4439e875fa60697f5361d56de58f
Piyush-Jadhav2804 <71430828+Piyush-Jadhav2804@users.noreply.github.com>
2023-06-06 15:13:34 +0530

    added logger middleware using currying suntax

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { legacy_createStore } from "redux";
+import { legacy_createStore, applyMiddleware } from "redux";
 
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = legacy_createStore(rootReducer);
+// const logger = function({ dispatch, getState }) {
+//   return function(next) {
+//     return function(action) {
+//       //middleware code
+
+//       console.log('ACTION_TYPE = ', action.type);
+//       next(action);
+//     }
+//   }
+// }
+
+const logger = ({dispatch, getState}) => (next) => (action) => {
+  console.log("ACTION_TYPE = ", action.type);
+  next(action);
+}
+
+const store = legacy_createStore(rootReducer, applyMiddleware(logger ));
 console.log('store:',store);
 // console.log('Prev State: ', store.getState());
 
